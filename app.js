2f953ee1dbd0eb0a653e93ff2687f46eb57deb03
BadGuerl <mi_ailis@hotmail.com>
2021-01-15 19:50:18 +0100

    done

--- app.js
+++ app.js
@@ -26,15 +26,42 @@ const spotifyApi = new SpotifyWebApi({
 const router = require('./config/routes.js');
 app.use('/', router);
 
-app.get('/artist-search', (req, res) => {
+app.get('/artist-search-results', (req, res) => {
   spotifyApi
-  .searchArtists(req.query.search)
+  .searchArtist(req.query.search)
   .then(data => {
     console.log('The received data from the API: ', data.body);
     // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-    res.render('artist-search-results', { data: data.body.artists} )
+    res.render('search', { data: data.body.artist} )
   })
   .catch(err => console.log('The error while searching artists occurred: ', err));
 })
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi
+  .getArtistAlbums(req.params.artistId)
+  .then(data => {
+      res.render('albums', {albums: data.body.items});
+      const dataToDisplay = {
+          searchResults: data.body
+      };
+      res.render('albums', dataToDisplay);
+      })
+      .catch(err => console.log('The error while searching albums occurred: ', err));
+});
+
+app.get('/albums/:albumId/tracks', (req, res, next) => {
+  // Get tracks in an album
+  spotifyApi
+  .getAlbumTracks(req.params.albumId)
+  .then(data => {
+    const tracks = data.body.items;
+      console.log('Tracks ====================', tracks);
+      res.render('tracks', { tracks } );
+    })
+  .catch(err => console.log('The error while searching tracks occurred: ', err));
+});
+
+module.exports = app;
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
